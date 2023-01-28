f3fd96b5095c6aac3c0f55abc5a079f758956739
FilosofDanil <serhio3347@gmail.com>
2023-01-28 20:02:04 +0100

    test

--- java/src/main/java/backend/educationproject/EducationProjectApplication.java
+++ java/src/main/java/backend/educationproject/EducationProjectApplication.java
@@ -1,4 +1,4 @@
-package java.backend.educationproject;
+package backend.educationproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
