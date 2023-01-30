3115546e803bcbe4e18112c43eee15978a02d29e
FilosofDanil <serhio3347@gmail.com>
2023-01-30 20:46:45 +0100

    fixes

--- java/src/main/java/backend/educationproject/EducationProjectApplication.java
+++ java/src/main/java/backend/educationproject/EducationProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EducationProjectApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EducationProjectApplication.class, args);
     }
