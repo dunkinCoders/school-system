1011777fbe9243f7692cf28521e0c2471a118f00
Nazariy <nazar260804@gmail.com>
2023-02-24 15:04:55 +0200

    added font awesome icons

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, fas);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
